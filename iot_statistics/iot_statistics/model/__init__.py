4c0f6f23b59512dadadbdaf1ada7489b6ea6d0c1
Walter Pachlinger <walter.pachlinger@gmail.com>
2021-02-25 07:10:40 +0100

    V 0.0.dev5 (2021-02-25): added distribution files

--- iot_statistics/iot_statistics/model/__init__.py
+++ iot_statistics/iot_statistics/model/__init__.py
@@ -12,27 +12,3 @@
     either express or implied. See the LICENSE for the specific language governing permissions
     and limitations under the LICENSE.
 """
-# pylint: disable=wrong-import-position
-import sys
-from flask import Flask
-app = Flask(__name__)
-
-if __file__.rfind('\\') < 0:
-    DELIMITER = '/'
-else:
-    DELIMITER = '\\'
-
-current_dir = __file__[:__file__.rfind(DELIMITER) - len(__file__)]
-if current_dir not in sys.path:
-    sys.path.append(current_dir)
-model_dir = f"{current_dir}{DELIMITER}model"
-if model_dir not in sys.path:
-    sys.path.append(model_dir)
-view_dir = f"{current_dir}{DELIMITER}view"
-if view_dir not in sys.path:
-    sys.path.append(view_dir)
-
-import views
-import model.stat_generator
-import model.stat_input_probe
-import view.stat_view
--- iot_statistics/setup.py
+++ iot_statistics/setup.py
@@ -4,21 +4,26 @@ with open("README.md", "r", encoding="utf-8") as fh_readme:
     long_description = fh_readme.read()
 
 setuptools.setup(
-    name = "iot_web_app",
-    version = "0.0.dev4",
+    name = "iot_statistics",
+    version = "0.0.dev5",
     author = "Walter Pachlinger",
     author_email = "walter.pachlinger@gmail.com",
-    description = "Web applications for the Raspberry Pi based IOT system.",
+    description = "Inspect the contents of a statistics database (created by an IOT Recorder).",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "",
     packages = setuptools.find_packages(),
+    include_package_data = True,
+    zip_safe = False,
     classifiers = [
         "Programming Language :: Python :: 3",
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: European Union Public Licence 1.2 (EUPL 1.2)",
         "Operating System :: OS Independent"
+    ],
+    install_requires = [
+        'flask',
     ],
     python_requires = ">= 3.7"
 )
